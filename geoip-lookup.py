d369464f5e4a7950076de0527ad1e7266faa7cb3
Ralph Schmieder <rschmied@cisco.com>
2017-06-04 11:52:23 +0200

    works now for all users when used in channel, plus a few fixes

--- geoip-lookup.py
+++ geoip-lookup.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re, xchat, GeoIP
+import re
+import xchat
+import GeoIP
 
 
 __module_name__ = "geoip"
-__module_version__ = "0.1"
+__module_version__ = "0.2"
 __module_description__ = "Lookup location via GeoIP"
 __module_author__ = "ralph.schmieder@gmail.com"
 
@@ -14,13 +16,13 @@ gi = GeoIP.open("/usr/share/GeoIP/GeoIPCity.dat", GeoIP.GEOIP_STANDARD)
 
 
 def colordecode(word):
-   # taken from: http://xchatdata.net/Scripting/TextFormatting"
-   _B = re.compile('%B', re.IGNORECASE)  # bold
-   _C = re.compile('%C', re.IGNORECASE)  # color (takes parameter)
-   _R = re.compile('%R', re.IGNORECASE)  # reverse
-   _O = re.compile('%O', re.IGNORECASE)  # original color
-   _U = re.compile('%U', re.IGNORECASE)  # underline
-   return _B.sub('\002', _C.sub('\003', _R.sub('\026', _O.sub('\017', _U.sub('\037', word)))))
+    # taken from: http://xchatdata.net/Scripting/TextFormatting"
+    _B = re.compile('%B', re.IGNORECASE)  # bold
+    _C = re.compile('%C', re.IGNORECASE)  # color (takes parameter)
+    _R = re.compile('%R', re.IGNORECASE)  # reverse
+    _O = re.compile('%O', re.IGNORECASE)  # original color
+    _U = re.compile('%U', re.IGNORECASE)  # underline
+    return _B.sub('\002', _C.sub('\003', _R.sub('\026', _O.sub('\017', _U.sub('\037', word)))))
 
 
 def printLine(user, location):
@@ -47,22 +49,27 @@ def getLocationWhoisNameCB(word, word_eol, userdata):
 
 
 def getLocationCB(word, word_eol, userdata):
-    if len(word) < 2:
-        xchat.emit_print("Server Error", __module_name__.upper() + " :Not enough parameters")
+    all_users = not len(word) > 1
+    channel = xchat.get_info("channel")
+    if channel.startswith("#"):
+        users = xchat.get_list('users')
+        found = False or all_users
+        for user in users:
+            loc = user.host.split('@')[1]
+            if not all_users and not xchat.nickcmp(user.nick, word[1]):
+                found = True
+            if found:
+                printLocationLine(user.nick, loc)
+            if found and not all_users:
+                break
+        if not found:
+            xchat.emit_print(
+                "Server Error", "Can't find user %s" % word[1])
+    elif not all_users:
+        xchat.command('whois %s' % word[1])
     else:
-        channel = xchat.get_info("channel")
-        if channel.startswith("#"):
-            users = xchat.get_list('users')
-            found = False
-            for user in users:
-                if not xchat.nickcmp(user.nick, word[1]):
-                    found = True
-                    loc = user.host.split('@')[1] 
-                    printLocationLine(word[1], loc)
-            if not found:
-                xchat.emit_print("Server Error", "Can't find user %s" % word[1])
-        else:
-            xchat.command('whois %s' % word[1])
+        xchat.emit_print("Generic Message", "@",
+                         "no username given or not in a channel")
     return xchat.EAT_ALL
 
 
@@ -76,17 +83,20 @@ def unloadCB(userdata):
     print colordecode("%C4Plugin %B" + __module_name__ + "%B " + __module_version__ + " unloaded.")
 
 
-try:
-    cmd = __module_name__.upper()
-    xchat.hook_command(cmd, getLocationCB, help="/" + cmd + " <userid> Gets location of user")
-    xchat.hook_print("Join", getLocationJoinCB)
-    xchat.hook_print("Whois Name Line", getLocationWhoisNameCB)
-    xchat.hook_unload(unloadCB)
-    print colordecode("%C4Plugin %B" + __module_name__ + "%B " + __module_version__ + " loaded.")
+def setup():
+    try:
+        cmd = __module_name__.upper()
+        xchat.hook_command(
+            cmd, getLocationCB, help="""/{} [<userid>] Gets location of user(s).
+            When used in a channel, list location for all users in the channel.
+            Otherwise, print the location of the given user <userid>.""".format(cmd))
+        xchat.hook_print("Join", getLocationJoinCB)
+        xchat.hook_print("Whois Name Line", getLocationWhoisNameCB)
+        xchat.hook_unload(unloadCB)
+        print colordecode("%C4Plugin %B" + __module_name__ + "%B " + __module_version__ + " loaded.")
 
-except:
-    print "can't hook callbacks to xchat"
+    except:
+        print "can't hook callbacks to xchat"
 
 if __name__ == '__main__':
-    pass
-
+    setup()
